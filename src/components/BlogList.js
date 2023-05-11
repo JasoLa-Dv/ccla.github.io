2d9273f47650308c9f86ce920778a297176917ec
juan silva <juan_silva_o@hotmail.com>
2023-05-11 14:49:27 -0400

    Bloglist

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,191 @@
 
-
 function BlogListExample() {
     return (
 
+<div className="container mt-4 mb-lg-5 pt-lg-2 pb-5">
+<div class="row align-items-end gy-3 mb-4 pb-lg-3 pb-1">
+<div class="row align-items-end gy-3 mb-4 pb-lg-3 pb-1">
+  <div class="col-lg-5 col-md-4">
+    <h1 class="mb-2 mb-md-0">Comisiones</h1>
+  </div>
+</div>
 
 
-<div></div>
+
+  <article class="card border-0 shadow-sm overflow-hidden mb-4">
+  <div class="row g-0"  >
+
+  <div class="col-sm-4 " >
+
+    
+  <img className="d-block w-50 rounded-3"
+          src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1683827218/Shoe/comunicacion_bjyxhf.jpg"
+          alt="First slide"/> 
+    </div>
+
+
+    <div class="col-sm-8">
+      <div class="card-body">
+        <div class="d-flex align-items-center mb-2">
+          <a href="https://home.jpg" class="badge bg-secondary text-decoration-none">Comisión de Comunicaciones y RR. SS.</a>
+          <span class="fs-sm text-muted border-start ps-3 ms-3">Sep 3, 2021</span>
+        </div>
+       
+        <p>Buscamos personas entusiatas que quieran ser parte de la Comisión de Comunicaciones y RR. SS. de nuestro CGPA..</p>
+        <hr class="my-4" />
+        <div class="d-flex align-items-center justify-content-between">
+          <a href="https://home.jpg" class="d-flex align-items-center fw-bold text-dark text-decoration-none me-3">
+            <img src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1670009941/Shoe/avatar_ywkhdt.png" class="rounded-circle me-3" width="48" alt="Avatar" />
+            Juan Silva Oróstica
+          </a>
+          
+        </div>
+      </div>
+    </div>
+</div>
+<hr class="featurette-divider" />
+
+
+  </article>
+
+  <article class="card border-0 shadow-sm overflow-hidden mb-4">
+  <div class="row g-0">
+  <div class="col-sm-4 mx-auto" >
+  <img className="d-block w-50  align-items-center rounded-3"
+          src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1683826378/Shoe/educacion_xpjibx.jpg"
+          alt="First slide"/> 
+    </div>
+    <div class="col-sm-8">
+      <div class="card-body">
+        <div class="d-flex align-items-center mb-3">
+          <a href="https://home.jpg" class="badge fs-sm text-nav bg-secondary text-decoration-none">Comisión de Educación</a>
+          <span class="fs-sm text-muted border-start ps-3 ms-3">Sep 3, 2021</span>
+        </div>
+       
+        <p>Un niño que lee será un niño que piensa, lo dijo algún genial pensador y no se equivocaba. Leer es juego, es entretenimiento, es construir sueños, es reflexionar, es un estado de ánimo, es aislamiento y compañía, es placer.</p>
+        <hr class="my-4" />
+        <div class="d-flex align-items-center justify-content-between">
+          <a href="https://home.jpg" class="d-flex align-items-center fw-bold text-dark text-decoration-none me-3">
+            <img src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1670009941/Shoe/avatar_ywkhdt.png" class="rounded-circle me-3" width="48" alt="Avatar" />
+            Marcelo Lobos del Canto</a>
+            </div>
+      </div>
+    </div>
+</div>
+</article>
+
+<hr class="featurette-divider" />
+
+<article class="card border-0 shadow-sm overflow-hidden mb-4">
+  <div class="row g-0">
+  <div class="col-sm-4 mx-auto" >
+  <img className="d-block w-50  align-items-center rounded-4"
+          src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1683827091/Shoe/medio_ambiente_kdf4xd.jpg"
+          alt="First slide"/> 
+    </div>
+    <div class="col-sm-8">
+      <div class="card-body">
+        <div class="d-flex align-items-center mb-3">
+          <a href="https://home.jpg" class="badge fs-sm text-nav bg-secondary text-decoration-none">Comisión de Medio Ambiente y Recreación</a>
+          <span class="fs-sm text-muted border-start ps-3 ms-3">Sep 3, 2021</span>
+        </div>
+       
+        <p>*El Centro de Padres de CCLA*, invita a toda la comunidad a continuar con la recolección de tapitas plásticas. El contenedor ya está instalado en el hall. ♻️ Reducir la basura es tarea de todos!!! y de esta manera ayudaremos a los niños con cancer Atte,</p>
+        <hr class="my-4" />
+        <div class="d-flex align-items-center justify-content-between">
+          <a href="https://home.jpg" class="d-flex align-items-center fw-bold text-dark text-decoration-none me-3">
+            <img src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1670009941/Shoe/avatar_ywkhdt.png" class="rounded-circle me-3" width="48" alt="Avatar" />
+            Laura Herrera Lagos</a>
+            </div>
+      </div>
+    </div>
+</div>
+</article>
+<hr class="featurette-divider" />
+
+<article class="">
+  <div class="row g-0">
+  <div class="col-sm-4" >
+  <img className="w-50 rounded-4"
+          src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1683828591/Shoe/Deporte_wtesmw.jpg"
+          alt="First slide"/> 
+    </div>
+    <div class="col-sm-8">
+      <div class="card-body">
+        <div class="d-flex align-items-center mb-3">
+          <a  href="https://home.jpg" class="badge fs-sm text-nav bg-secondary text-decoration-none">Comisión de Deportes y Recreación</a>
+          <span class="fs-sm text-muted border-start ps-3 ms-3">Sep 3, 2021</span>
+        </div>
+       
+        <p>¡Buenos días! Les compartimos algunas fotos de la actividad de anoche de power jump ¡Quedan todos cordialmente invitados a participar todos los jueves a las 20:00 horas!</p>
+        <hr class="my-4" />
+        <div class="d-flex align-items-center justify-content-between">
+          <a href="https://home.jpg" class="d-flex align-items-center fw-bold text-dark text-decoration-none me-3">
+            <img src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1670009941/Shoe/avatar_ywkhdt.png" class="rounded-circle me-3" width="48" alt="Avatar" />
+            Francisco Sánchez Vera</a>
+            </div>
+      </div>
+    </div>
+</div>
+</article>
+<hr class="featurette-divider" />
+
+<article class="">
+  <div class="row g-0">
+  <div class="col-sm-4" >
+  <img className="w-50 rounded-4"
+          src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1683828599/Shoe/bienestar_fimvvk.jpg"
+          alt="First slide"/> 
+    </div>
+    <div class="col-sm-8">
+      <div class="card-body">
+        <div class="d-flex align-items-center mb-3">
+          <a  href="https://home.jpg" class="badge fs-sm text-nav bg-secondary text-decoration-none">Comisión de Bienestar</a>
+          <span class="fs-sm text-muted border-start ps-3 ms-3">Sep 3, 2021</span>
+        </div>
+       
+        <p>Estimados, buenas tardes, ese es el afiche, del lanzamiento oficial de nuestra campaña, como comunidad Colegio Concepción Los Ángeles. Desde ya, muchas gracias a todos, por ser parte de esta campaña, y por su invaluable cooperación.</p>
+        <hr class="my-4" />
+        <div class="d-flex align-items-center justify-content-between">
+          <a href="https://home.jpg" class="d-flex align-items-center fw-bold text-dark text-decoration-none me-3">
+            <img src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1670009941/Shoe/avatar_ywkhdt.png" class="rounded-circle me-3" width="48" alt="Avatar" />
+            Verónica Ortiz López</a>
+            </div>
+      </div>
+    </div>
+</div>
+</article>
+<hr class="featurette-divider" />
+
+
+<article class="">
+  <div class="row g-0">
+  <div class="col-sm-4" >
+  <img className="w-50 rounded-4"
+          src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1683828614/Shoe/convivencia_rtvar0.jpg"
+          alt="First slide"/> 
+    </div>
+    <div class="col-sm-8">
+      <div class="card-body">
+        <div class="d-flex align-items-center mb-3">
+          <a  href="https://home.jpg" class="badge fs-sm text-nav bg-secondary text-decoration-none">Comisión de Convivencia</a>
+          <span class="fs-sm text-muted border-start ps-3 ms-3">Sep 3, 2021</span>
+        </div>
+       
+        <p>BIENVENIDA PRIMAVERA!🌸 LOS APODERADOS SI PUEDEN ASISTIR Y ACOMPAÑAR A SUS HIJOS.👨‍👩‍👧👨‍👩‍👦 •Se entregará comunicación junto con autorización, desde 1º basico hasta 7º basico. Teniendo como objetivo tener el permiso de padres para que alumnos puedan asistir después de clases.🌈 •Habrá un puesto que venderá bebestibles y snacks! 🥤🧃🍪🍓🍊🍎🥪🍭🧁 • Trae bloqueador solar y emoción!</p>
+        <hr class="my-4" />
+        <div class="d-flex align-items-center justify-content-between">
+          <a href="https://home.jpg" class="d-flex align-items-center fw-bold text-dark text-decoration-none me-3">
+            <img src="https://res.cloudinary.com/ddahjf0hr/image/upload/v1670009941/Shoe/avatar_ywkhdt.png" class="rounded-circle me-3" width="48" alt="Avatar" />
+            Edson Araneda Araneda</a>
+            </div>
+      </div>
+    </div>
+</div>
+</article>
+<hr class="featurette-divider" />
+</div>
+</div>
 
       );
 }
